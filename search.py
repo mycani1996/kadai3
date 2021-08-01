8ccd5fda749dfbaa7cea7a1b86d7c0f63ac124e0
mycani1996 <mycani1996@gmail.com>
2021-08-01 20:33:33 +0900

    first

---
+++ search.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import eel
+
+### デスクトップアプリ作成課題
+def kimetsu_search(word):
+    print_txt:str
+    # 検索対象取得
+    df=pd.read_csv("./source.csv")
+    source=list(df["name"])
+
+    # 検索
+    if word in source:
+        print_txt = "『{}』はあります".format(word)
+    else:
+        print_txt = "『{}』はありません、追加します".format(word)
+        source.append(word)
+    
+    # CSV書き込み
+    df=pd.DataFrame(source,columns=["name"])
+    df.to_csv("./source.csv",encoding="utf_8-sig")
+    eel.view_log_js(print_txt)
